b1e06df373318126c0f047942fe62d4f3ea0a9ff
ahmed <ahmedhalwgy@gmail.com>
2024-02-27 08:52:27 +0200

    All gategory routes handeled

--- src/controllers/categoryRoutes.ts
+++ src/controllers/categoryRoutes.ts
@@ -33,3 +33,79 @@ export const createCategory = catchAsync(
     });
   }
 );
+
+export const getAllCategories = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const categories = await prisma.category.findMany({});
+
+    if (categories.length === 0) {
+      return next(new AppError('no categories found.', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      daa: {
+        categories,
+      },
+    });
+  }
+);
+
+export const getCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) return next(new AppError('cayegory not found.', 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        category,
+      },
+    });
+  }
+);
+
+export const updateCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) return next(new AppError('category not found', 404));
+
+    const { error } = createCategoryValidation(req.body);
+    if (error) return next(new AppError(error.details[0].message, 400));
+
+    const updatedCategory = await prisma.category.update({
+      where: { id },
+      data: {
+        name: req.body.name,
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        updatedCategory,
+      },
+    });
+  }
+);
+
+export const deleteCategory = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    const category = await prisma.category.findUnique({ where: { id } });
+
+    if (!category) return next(new AppError('category not found.', 404));
+
+    await prisma.category.delete({ where: { id } });
+
+    res.status(204).json({});
+  }
+);
--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import { protect } from '../middlewares/protectMiddleware';
 import { restrictToAdmin } from '../middlewares/authorizationMiddleware';
-import { createCategory } from '../controllers/categoryRoutes';
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategory,
+  updateCategory,
+} from '../controllers/categoryRoutes';
 
 export const catrgoryRouter = express.Router();
 
 catrgoryRouter.use(protect);
 // catrgoryRouter.use(restrictToAdmin);
-catrgoryRouter.route('/').post(createCategory);
+catrgoryRouter.route('/').post(createCategory).get(getAllCategories);
+
+catrgoryRouter
+  .route('/:id')
+  .get(getCategory)
+  .patch(updateCategory)
+  .delete(deleteCategory);
